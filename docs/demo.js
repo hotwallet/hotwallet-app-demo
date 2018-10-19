8b215539b0043088e02547b6235494af021757f9
Will Schmid <will123195@gmail.com>
2018-10-18 22:16:06 -0600

    Update demo.js

--- docs/demo.js
+++ docs/demo.js
@@ -4,7 +4,6 @@ HotWallet.getTransactions()
   .then(function (transactions) {
     var html = [
       '<div style="padding: 25px">',
-      '  <h1 id="h1" style="padding: 0; margin: 0;">Demo App</h1>',
       '  <p>You have ' + transactions.length + ' transactions.</p>',
       '  <button onclick="document.getElementById(\'h1\').innerHTML = document.getElementById(\'h1\').innerHTML + \'<div style=height:200>.</div>\'">Button</button>',
       '</div>'
