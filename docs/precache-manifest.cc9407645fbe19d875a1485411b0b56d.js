fa7957e4364cd80d0c8836616ffeab7ad4b50f4e
will123195 <will123195@gmail.com>
2018-10-21 12:03:12 -0600

    fix homepage

---
+++ docs/precache-manifest.cc9407645fbe19d875a1485411b0b56d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a96ec1bd4e7e624c34",
+    "url": "/hotwallet-app-news/static/js/runtime~main.42a96ec1.js"
+  },
+  {
+    "revision": "07fc22aa844439109258",
+    "url": "/hotwallet-app-news/static/js/main.07fc22aa.chunk.js"
+  },
+  {
+    "revision": "d874d09434a4abdeb137",
+    "url": "/hotwallet-app-news/static/js/1.d874d094.chunk.js"
+  },
+  {
+    "revision": "07fc22aa844439109258",
+    "url": "/hotwallet-app-news/static/css/main.72959f85.chunk.css"
+  },
+  {
+    "revision": "cb1f9806cc30c3eee3c3cb629a247ebf",
+    "url": "/hotwallet-app-news/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0360ebe3405a7b05ac50e48d82623f4e.js"
+  "/hotwallet-app-news/precache-manifest.cc9407645fbe19d875a1485411b0b56d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hotwallet-app-news/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
